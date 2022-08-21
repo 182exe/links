a4eb5e8f57a7053820b6e071fe0de70200f59a7a
182exe <81325987+182exe@users.noreply.github.com>
2022-08-21 14:04:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 idList = [
     "youtube",
     "discord",
@@ -9,13 +10,81 @@ urlList = [
 ];
 packList = [
     "purplesynthwave-32",
+    "aka-16",
+    "igetis-32",
+    "blazn-32",
+    "dystopia-32",
+    "deepred-128",
+    "rustymagma-32",
+    "emofault-16",
+    "zigzag-64",
+    "royalblueuhc-16",
+    "hauptkommisar-32",
+    "diamondocean-16",
+    "oceanic-16",
+    "redcloudsv2-512",
+    "vermillion-16",
+    "yoifault-16",
+    "blueice-32",
+    "ultimatefaultv2-16",
+    "zigzagv2-64",
+    "tapsoundpack",
+    "devilishskyoverlay",
+    "cloudy-32",
+    "calamity-16",
+    "hotcocoadefault-16",
+    "hotcocoa-16",
+    "182sfpsblocks-16",
+    "bluemoonrevamp-32",
+    "christmascookies-16",
+    "jfault-16",
+    "redclouds-512",
+    "ultimatefault-16"
+    "vibrantfaithful-32",
+    "1.14faith-64",
+    "templateblockhit",
+    "defaulttextures-16",
 ];
 packUrlList = [
     "https://www.mediafire.com/file/8hvbolmsjltofxk/%2521_%25C2%25A75Purple_%25C2%25A76Synthwave_%25C2%25A72%255B32x%255D%25C2%25A77.zip/file",
+    "https://www.mediafire.com/file/1yedk8kf55s31p6/%2521_%25C2%25A7cAka.zip/file",
+    "https://www.mediafire.com/file/hwinwq6urnmbrt0/%2521_%25C2%25A79%25C2%25A7lIgetis_%25C2%25A71%255B32x%255D.zip.zip/file",
+    "https://www.mediafire.com/file/0r9k8hn2kosxyow/%2521_%25C2%25A7cBl%25C2%25A76az%25C2%25A7en_%25C2%25A7f%255B32x%255D%25C2%25A77.zip/file",
+    "https://www.mediafire.com/file/ipk5bx220levwg1/%2521_%25C2%25A74Dystopia.zip/file",
+    "https://www.mediafire.com/file/fpera252xheanb8/%2521_%2523%25C2%25A74%25C2%25A7lDeep_Red.zip/file",
+    "https://www.mediafire.com/file/61diishoq6i7no3/%2521_%25C2%25A76%25C2%25A7lRusty%25C2%25A7e%25C2%25A7lMagma_%25C2%25A77%25C2%25A7o%255B32x%255D.zip/file",
+    "https://www.mediafire.com/file/wlpjn980ajp6jo8/%2521_%25C2%25A78%25C2%25A7lEmo%25C2%25A73%25C2%25A7lFault_%25C2%25A77%255B%25C2%25A7e16x%25C2%25A77%255D.zip/file",
+    "https://www.mediafire.com/file/c6e5ycw88zrc0ti/%2521_%25C2%25A7cZigZag_%255B64x%255D.zip/file",
+    "https://www.mediafire.com/file/e4ti5ve7c9lqu1y/%2521_%25C2%25A7dRoyal_Blue_%25C2%25A7eUHC_%25C2%25A77%255B16x%255D.zip/file",
+    "https://www.mediafire.com/file/3w7w5ukphkwsx1n/%2521_%25C2%25A7aHaupt%25C2%25A75kommisar%25C2%25A77_%255B32x%255D.zip/file",
+    "https://www.mediafire.com/file/pz6ecqm98sz07qk/%2521_%25C2%25A7b%25C2%25A7lDiamond_%25C2%25A73%25C2%25A7lOcean_%25C2%25A77%25C2%25A7l%255B16x%255D.zip/file",
+    "https://www.mediafire.com/file/96vhnzupw6x0wu6/%2521_%25C2%25A73Ocea%25C2%25A7bnic_%25C2%25A7616x%25C2%25A77.zip/file",
+    "https://www.mediafire.com/file/jxihh9oy71gbwdu/%2521_%25C2%25A74Red_%25C2%25A7cClouds_%25C2%25A7f%255B512x%255D_%25C2%25A77v2.zip/file",
+    "https://www.mediafire.com/file/r1hvtsszyqpfkqv/%2521_%25C2%25A74Vermi%25C2%25A7cllion_%25C2%25A7716x%25C2%25A77.zip/file",
+    "https://www.mediafire.com/file/ym3tt97jtrfzb6z/%2521_%25C2%25A7cYoiFault_%25C2%25A77%255B16x%255D.zip/file",
+    "https://www.mediafire.com/file/bq7k9n7ox70sl6i/%2521_Blue_Ice_%255B32x%255D.zip/file",
+    "https://www.mediafire.com/file/rn54oass25u1sdq/%2521_UltimateFault_v2_%255B16x%255D.zip/file",
+    "https://www.mediafire.com/file/hug86c5vimf4v3n/%2521_ZigZag_v2_%255B64x%255D.zip/file",
+    "https://www.mediafire.com/file/s7w7wa8w1her6d4/%2521_%25C2%25A7fT%25C2%25A77a%25C2%25A78p_%25C2%25A7fS%25C2%25A7eo%25C2%25A76u%25C2%25A7cn%25C2%25A74d%25C2%25A75p%25C2%25A71a%25C2%25A73c%25C2%25A7bk%25C2%25A70.zip/file",
+    "https://www.mediafire.com/file/rvrcpzli1yzhilx/%2521_%25C2%25A7l%25C2%25A7dD%25C2%25A75e%25C2%25A74vi%25C2%25A7cli%25C2%25A76s%25C2%25A7eh_%25C2%25A7o%25C2%25A77Sky_Overlay.zip/file",
+    "https://www.mediafire.com/file/46ozyn9zuc2ajno/%2521_%25C2%25A7sb%25C2%25A7lCloudy.zip/file",
+    "https://www.mediafire.com/file/ixcof63gjrqr7la/%2521_Calamity_%255B16x%255D.zip/file",
+    "https://www.mediafire.com/file/8rg097inbwekkq6/%2521_Hot_Cocoa_%255B16x%255D_Default.zip/file",
+    "https://www.mediafire.com/file/w3ooj2t8u6bmf5n/%2521_Hot_Cocoa_%255B16x%255D.zip.zip/file",
+    "https://www.mediafire.com/file/fzuc55dqtlufmwc/%2521__182%2527s_FPS_Blocks.zip/file",
+    "https://www.mediafire.com/file/s84bpnm18anz93i/%25C2%25A71Blue_%25C2%25A79Moon_%25C2%25A7r%25C2%25A7o%255B32x%255D_%25C2%25A7eRevamp.zip/file",
+    "https://www.mediafire.com/file/ip29s5l8yxk6f8t/%25C2%25A72%25C2%25A7lChrist%25C2%25A7c%25C2%25A7lmas_%25C2%25A76%25C2%25A7lCookies_%25C2%25A77v1.1.zip/file",
+    "https://www.mediafire.com/file/xd4pqlrhcwnb0fa/%25C2%25A73%25C2%25A7lJFault_%25C2%25A7e%255B16x%255D_%25C2%25A77v1.1.zip/file",
+    "https://www.mediafire.com/file/xin5iigns3wap90/%25C2%25A74Red_%25C2%25A7cClouds_%25C2%25A7r%255B512x%255D.zip/file",
+    "https://www.mediafire.com/file/a7lrp72r150u994/%25C2%25A7e%25C2%25A7lUltimate%25C2%25A7d%25C2%25A7lFault_%25C2%25A7e%25C2%25A7l%255B16x%255D.zip/file",
+    "https://www.mediafire.com/file/x010i96lms133he/%25C2%25A7e%25C2%25A7lVibrant_%25C2%25A72%25C2%25A7lFaithful_%25C2%25A77%255B32x%255D.zip/file",
+    "https://www.mediafire.com/file/xzi7d7gcm5z1qpr/%25C2%25A7e1.14_%25C2%25A72%25C2%25A7Faith_%25C2%25A76%255B64x%255D_%25C2%25A77%255Bv2.5%255D.zip/file",
+    "https://www.mediafire.com/file/emv8oy1tlqxm0ib/%2521_Blockhit_Fix_%255B182%2527s_Template%255D.zip/file",
+    "https://www.mediafire.com/file/82sjodi19ntzc3b/Default_Textures_%255B1.8.9%255D.zip/file",
 ];
 
-var id = new URLSearchParams(window.location.search).get('id');
-var pack = new URLSearchParams(window.location.search).get('pack');
+let id = new URLSearchParams(window.location.search).get('id');
+let pack = new URLSearchParams(window.location.search).get('pack');
 let htmlObj = document.getElementById('display');
 htmlObj.innerHTML = 'Redirecting you to a link with ID: "' + id + '"';
 
