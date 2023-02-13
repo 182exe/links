37644e0adc8afb3c0fbc01b8ee4d3d52a4b31bb1
182exe <81325987+182exe@users.noreply.github.com>
2023-02-12 21:05:58 -0700

    Add mobile support
    Added eventlisteners for touching and clicking to allow mobile users to tap the screen to continue.

--- main.js
+++ main.js
@@ -33,18 +33,22 @@ if (Object.keys(params).length === 2) {
     var url = 'https://182exe.xyz';
 }
 
+function redirectSequence() {
+    document.body.classList.add('blackness');
+    [...document.querySelectorAll('.bgi')].forEach(element => {
+        element.classList.add('blackness');
+    })
+    const animated = document.querySelectorAll('.floatup');
+    [...animated].forEach(element => {
+        element.classList.replace('floatup', 'floatdown');
+    }); 
+    setTimeout(() => {
+        window.location.replace(url);
+    }, 1000);
+};
+
 setTimeout(() => {
-    window.addEventListener('keypress', () => {
-        document.body.classList.add('blackness');
-        [...document.querySelectorAll('.bgi')].forEach(element => {
-            element.classList.add('blackness');
-        })
-        const animated = document.querySelectorAll('.floatup');
-        [...animated].forEach(element => {
-            element.classList.replace('floatup', 'floatdown');
-        }); 
-        setTimeout(() => {
-            window.location.replace(url);
-        }, 1000);
-    });
+    window.addEventListener('keypress', () => { redirectSequence() });
+    window.addEventListener('touchstart', () => { redirectSequence() })
+    window.addEventListener('click', () => { redirectSequence() })
 }, 1000);
